f06f6e2ab03d2e1839b70c4b71fe31bee64616ba
franciscohanna92 <franciscohanna92@gmail.com>
2018-08-16 23:24:12 -0300

    fix

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex h-screen bg-purple p-5">
     <div class="self-end bg-transparent rounded mx-auto shadow-">
-      <div class="p-10 pb-5">
-        <img src="img/clipboard.svg" class="shadow-lg rounded-full" height="100" alt="">
+      <div class="px-12 py-8">
+        <img src="img/clipboard.svg" class="shadow-lg rounded-full" alt="">
       </div>
       <h2 class="text-white text-center mb-2">Bievenido a Pendlist</h2>
       <p class="text-center text-grey-light mb-10">Inicia sesión y organiza tus cosas!</p>
